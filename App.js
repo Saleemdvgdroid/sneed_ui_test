64df70463df5c802cdece972e5c12209176ee76b
Muhammed Saleem <muhammed.s@flamencotech.com>
2019-12-04 04:42:53 +0530

    added all files

--- App.js
+++ App.js
@@ -3,11 +3,6 @@ import {SafeAreaView,Text} from 'react-native';
 import FAB from './src/components/FAB';
 import ScreenOne from './src/screens/ScreenOne';
 import ScreenTwo from './src/screens/ScreenTwo';
-
-import { library } from '@fortawesome/fontawesome-svg-core'
-import {fas, faSearch,faHeart,faRedoAlt,faEllipsisV } from '@fortawesome/free-solid-svg-icons'
-library.add(fas,faSearch,faHeart,faRedoAlt,faEllipsisV);
-
 class App extends React.Component{
   render(){
     return(
